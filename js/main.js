abcd5096f599de102d76cf2bb5754b6f95c961da
YunKukPark <a01043340999@gmail.com>
2021-05-28 01:27:22 +0900

    🚧  Implement scroll Animation about calculate scrollRatio

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
   let yOffset = 0; // window.pageYOffset 대신 쓸 변수
   let prevScrollHeight = 0; // 현재 스크롤 위치 (Yoffset) 보다 이전에 위치한 스크롤 섹션 스크롤 높이의 합
   let currentScene = 0; // 현재 활성화된 Scene
+  let enterNewScene = false; // 새로운 scene이 시작되는 찰나의 순간 true
 
   const sceneInfo = [
     {
@@ -11,6 +12,13 @@
       scrollHeight: 0,
       objs: {
         container: document.querySelector('#scroll-section-0'),
+        messageA: document.querySelector('#scroll-section-0 .main-message.a'),
+        messageB: document.querySelector('#scroll-section-0 .main-message.b'),
+        messageC: document.querySelector('#scroll-section-0 .main-message.c'),
+        messageD: document.querySelector('#scroll-section-0 .main-message.d'),
+      },
+      values: {
+        messageA_opacity: [0, 1],
       },
     },
     {
@@ -62,26 +70,68 @@
     document.body.setAttribute('id', `show-scene-${currentScene}`);
   }
 
+  function calcValues(values, currentYOffset) {
+    let rv;
+    // 현재 씬(스크롤섹션)에서 스크롤 된 범위를 비율로 구하기
+    let scrollRatio = currentYOffset / sceneInfo[currentScene].scrollHeight;
+
+    rv = values[0] + scrollRatio * (values[1] - values[0]);
+    // 진행 수직선 시작점(values 배열 첫 값) -----*---------------- 끝점(values 배열 마지막 값)
+    // * => scrollRatio
+
+    return rv;
+  }
+
+  function playAnimation() {
+    const objs = sceneInfo[currentScene].objs;
+    const values = sceneInfo[currentScene].values;
+    const currentYOffset = yOffset - prevScrollHeight;
+    switch (currentScene) {
+      case 0:
+        let messageA_opacity_in = calcValues(
+          values.messageA_opacity,
+          currentYOffset
+        );
+        objs.messageA.style.opacity = messageA_opacity_in;
+        console.log(messageA_opacity_in);
+        let messageA_opacity_1 = values.messageA_opacity[1];
+
+        break;
+      case 1:
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+    }
+  }
+
   function scrollLoop() {
     // prevScroll => 이전 Scene 까지의 Scroll 양
     // sceneInfo[currentScene].scrollHeight => 이번 Scene의 Scroll 양
     // yOffset => Document의 최상단 부터 내려온 scroll의 양
-
+    enterNewScene = false;
     prevScrollHeight = 0;
     for (let i = 0; i < currentScene; i++) {
       prevScrollHeight += sceneInfo[i].scrollHeight;
     }
 
     if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+      enterNewScene = true;
       currentScene++;
+      document.body.setAttribute('id', `show-scene-${currentScene}`);
     }
 
     if (yOffset < prevScrollHeight) {
+      enterNewScene = true;
       if (currentScene === 0) return;
       currentScene--;
+      document.body.setAttribute('id', `show-scene-${currentScene}`);
     }
 
-    document.body.setAttribute('id', `show-scene-${currentScene}`);
+    if (enterNewScene) return; //scene이 바뀔 때 비정상적인 출력물 발생!  오동작 요인 사전 차단
+
+    playAnimation();
   }
 
   window.addEventListener('scroll', () => {
