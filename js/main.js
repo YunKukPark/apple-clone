26801589fe4f597d259d8ebe5ed27b95efdaf2cc
YunKukPark <a01043340999@gmail.com>
2021-06-08 15:52:49 +0900

    🚧  Calculate canvas white rect x,y,width,height

--- js/main.js
+++ js/main.js
@@ -103,7 +103,10 @@
         images: [],
       },
 
-      values: {},
+      values: {
+        rect1X: [0, 0, { start: 0, end: 0 }],
+        rect2X: [0, 0, { start: 0, end: 0 }],
+      },
     },
   ];
 
@@ -316,6 +319,21 @@
         //console.log(`widthRatio => ${widthRatio} \t heightRatio => ${heightRatio} \n 결정값 => ${canvasScaleRatio}`);
         objs.canvas.style.transform = `scale(${canvasScaleRatio})`;
         objs.context.drawImage(objs.images[0], 0, 0);
+
+        // 캔버스 사이즈에 맞춰 가정한 innerWidth와 innerHeight
+        const recalculatedInnerWidth = window.innerWidth / canvasScaleRatio;
+        const recalculatedInnerHeight = window.innerHeight / canvasScaleRatio;
+
+        // 좌우 흰색 사각형 width, height 셋팅
+        const whiteRectWidth = recalculatedInnerWidth * 0.15;
+        values.rect1X[0] = (objs.canvas.width - recalculatedInnerWidth) / 2;
+        values.rect1X[1] = values.rect1X[0] - whiteRectWidth;
+        values.rect2X[0] = values.rect1X[0] + recalculatedInnerWidth - whiteRectWidth;
+        values.rect2X[1] = values.rect2X[0] + whiteRectWidth;
+        // 좌우 흰색 사각형 그리기
+        objs.context.fillRect(values.rect1X[0], 0, parseInt(whiteRectWidth), recalculatedInnerHeight);
+        objs.context.fillRect(values.rect2X[0], 0, parseInt(whiteRectWidth), recalculatedInnerHeight);
+
         break;
     }
   }
